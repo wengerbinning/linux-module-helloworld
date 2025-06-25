b4248f18a55cdcffbace6f3a0b37e08d548091c1
Wenbin Zheng <wbzheng@grandstream.cn>
2025-06-25 19:37:10 +0800

    2025--06025

--- helloworld.c
+++ helloworld.c
@@ -12,6 +12,17 @@
 #define debug(level, format, ...)
 #endif
 
+/* https://litux.nl/mirror/kerneldevel      opment/0672327201/ch16lev1sec6.html */
+
+static int disabled = 0;
+module_param(disabled, bool, 0644); //  byte, short, ushort, int, uint, long, ulong, charp, bool, or invbool
+MODULE_PARM_DESC(size, "The size in inches of the fishing pole " \
+                 "connected to this computer.");
+static unsigned int debug = DEFAULT_MAX_LINE_TEST;
+module_param_named(kmod-debug, debug, int, 0);
+
+module_param_array(fish, int, &nr_fish, 0444);
+
 static int helloworld_init(void) {
     debug(DEBUG_LEVEL_A, "run in %s", __func__);
 
@@ -25,7 +36,7 @@ static void helloworld_exit(void) {
     debug(DEBUG_LEVEL_A, "run in %s", __func__);
 
     // TODO: Do something for release.
-    
+
     debug(DEBUG_LEVEL_A, "return from %s", __func__);
 }
 
